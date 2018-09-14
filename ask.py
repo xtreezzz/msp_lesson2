79438ee5f48e2b33d4833e5d280721784c0dcc21
Andrey Kozachek <xtreezzz@gmail.com>
2018-09-15 00:06:04 +0300

    вынес константный словарик из функции поменял условие

--- ask.py
+++ ask.py
@@ -1,18 +1,21 @@
+LIB_ANSWER = {"Как дела?": "Хорошо!",
+              "Что делаешь?":
+                  "Программирую",
+              "Как мне выйти из цикла?":
+                  "Спросить меня 'Как мне выйти из цикла?'",
+              "Как мне выйти из VIM?":
+                  ":q!"}
+
+
 def ask_user(_request_question=None):
     if _request_question:
         while True:
-            lib_answer = {"Как дела?": "Хорошо!",
-                          "Что делаешь?":
-                              "Программирую",
-                          "Как мне выйти из цикла?":
-                              "Спросить меня 'Как мне выйти из цикла?'",
-                          "Как мне выйти из VIM?":
-                              ":q!"}
-            if lib_answer.get(_request_question, 'Wrong') == 'Wrong':
+            # if lib_answer.get(_request_question, 'Wrong') == 'Wrong':
+            if not LIB_ANSWER.get(_request_question):
                 _request_question \
                     = input('Так интересно, спроси меня что-нибудь ещё! ')
             else:
-                print(lib_answer[_request_question])
+                print(LIB_ANSWER[_request_question])
                 break
     else:
         while True:
