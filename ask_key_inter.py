a09d2632587883e5bfea9169c061a14ad203da97
Andrey Kozachek <xtreezzz@gmail.com>
2018-09-15 02:09:48 +0300

    Обработка выхода из программы, перехватываем и говорим пока

---
+++ ask_key_inter.py
@@ -0,0 +1,37 @@
+LIB_ANSWER = {"Как дела?": "Хорошо!",
+              "Что делаешь?":
+                  "Программирую",
+              "Как мне выйти из цикла?":
+                  "Спросить меня 'Как мне выйти из цикла?'",
+              "Как мне выйти из VIM?":
+                  ":q!"}
+
+
+def ask_user(_request_question=None):
+    if _request_question:
+        while True:
+            try:
+                # if lib_answer.get(_request_question, 'Wrong') == 'Wrong':
+                if not LIB_ANSWER.get(_request_question):
+                    _request_question \
+                        = input('Так интересно, спроси меня что-нибудь ещё! ')
+                else:
+                    print(LIB_ANSWER[_request_question])
+                    break
+            except KeyboardInterrupt:
+                print('Пока!')
+                break
+    else:
+        while True:
+            try:
+                answer = input("Как дела? ")
+                if answer == 'Хорошо':
+                    break
+            except KeyboardInterrupt:
+                print('Пока!')
+                break
+
+
+ask_user()
+request_question = input("Задай мне вопрос: ")
+ask_user(request_question)
