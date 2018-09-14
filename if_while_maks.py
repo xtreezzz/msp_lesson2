52f7c727cc1193154785d3610543f5a967331cea
Andrey Kozachek <xtreezzz@gmail.com>
2018-09-15 00:05:01 +0300

    Проверил, что у коллеги аналогичные результаты

--- if_while_maks.py
+++ if_while_maks.py
@@ -1,19 +1,27 @@
-five_school = [{'school_class': '4a', 'scores': [2, 4, 4, 5, 2]},
-               {'school_class': '4b', 'scores': [3, 4, 2, 5, 1]},
-               {'school_class': '5a', 'scores': [3, 4, 4, 5, 2]},
-               {'school_class': '3a', 'scores': [3, 1, 2, 5, 2]},
-               {'school_class': '3b', 'scores': [2, 4, 4, 3, 2]},
-               {'school_class': '6a', 'scores': [3, 2, 4, 5, 2]},
-               {'school_class': '3c', 'scores': [5, 4, 1, 5, 2]},
-               {'school_class': '6c', 'scores': [3, 5, 1]},
-               ]
+print('********IMPORTED FROM https://github.com/hronikum/lesson2/blob/master/homework/task_3.py**********')
+#:1: E402 module level import not at top of file --знаю но так нужно
+import task3
 
+print('********CASE1**********')
 school_sum = list()
 school_count = list()
-for classes in five_school:
+for classes in task3.school_scores:
     print(f"Класс {classes['school_class']}:"
           f" средний бал {sum(classes['scores'])/len(classes['scores'])}")
     school_sum.append(sum(classes['scores']))
     school_count.append(len(classes['scores']))
+
+print(f"Общий средний бал по школе: {sum(school_sum)/sum(school_count)}")
+
+print('********CASE2**********')
+
+school_sum = list()
+school_count = list()
+for classes in task3.school_scores:
+    school_count.append(len(classes['scores']))
+    print(f"Класс {classes['school_class']}:"
+          f" средний бал {sum(classes['scores'])/len(classes['scores'])}")
+    for marks in classes['scores']:
+        school_sum.append(marks)
 
 print(f"Общий средний бал по школе: {sum(school_sum)/sum(school_count)}")
