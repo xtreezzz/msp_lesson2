cfa546758794dc0659d0392ac3e6b3963986480a
Andrey Kozachek <xtreezzz@gmail.com>
2018-09-15 00:04:02 +0300

    Убрал избыточное условие

--- if.py
+++ if.py
@@ -2,17 +2,17 @@ _age = input("Введите возраст: ")
 int_age = int(_age)
 
 
-def chose_destiony(age):
+def choose_destiony(age):
     if age <= 6:
         dest = 'детский сад'
-    elif age > 6 and age <= 18:
+    elif age <= 18:
         dest = 'школа'
-    elif age > 18 and age <= 22:
+    elif age <= 22:
         dest = 'вуз'
     else:
         dest = 'работа'
     return dest
 
 
-int_age_chose = chose_destiony(int_age)
+int_age_chose = choose_destiony(int_age)
 print(int_age_chose)
